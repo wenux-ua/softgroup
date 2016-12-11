26ab5009a05961d9c05f71be2dd5f13f162f4a2c
wenux-ua <vslgavrish@gmail.com>
2016-12-11 18:27:44 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,35 @@
+﻿<style type="text/css">
+	body {
+		background:#5685BF; 
+		color:#fff;
+		font-size:22px;
+		text-align:center;
+		}
+		
+		
+	H1 { 
+		display: block;
+		font-size:40px;
+		width:50%;
+		background: linear-gradient(to top, #5685BF, #143963);
+		color:#fff;
+		padding: 10px;
+		margin: 20px auto;
+		border: 1px solid #5685BF;
+		text-align: center;
+	}
+   
+
+  </style>
+<?php
+	include ("header.php");
+	
+$about_me = <<<ABOUT
+		<h2>Гавриш Василь Ігорович</h2>
+		<strong>Відвідувати заняття зручно у будь-який час.</strong>
+ABOUT;
+
+	echo "$about_me";	
+		
+?>
+
---
+++ header.php
@@ -0,0 +1,56 @@
+ <html>
+ <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		$("body").css("display", "none");
+		$("body").fadeIn(1000);
+		});
+	</script>
+ 
+ <head><title>Блог</title></head>
+<style>
+ header {
+  display: block;
+  font-size:22px;
+   background: linear-gradient(to top, #143963, #5685BF);
+   color:#fff;
+   link:#fff;
+    padding: 10px;
+    border: 1px solid #5685BF;
+	text-align: center;
+ }
+ 
+ A {
+    color: #cecece; /* Цвет ссылок */
+   }
+   A:visited {
+    color: #cecece; /* Цвет посещенных ссылок */
+   }
+   A:active {
+    color: #ffff00; /* Цвет активных ссылок */
+   }
+   
+</style>
+
+<header>
+<?php
+	$add_article='<a href="add.php">Добавити статтю</a>';
+	$about_me='<a href="about.php">Про мене</a>';
+		
+		echo '<h1>Блог</h1>';
+	
+	//навігаціця
+	$main_nav = array(
+		"index.php" => "&#8226; Блог",
+		"add.php" => "&#8226; Добавити статтю",
+		"about.php" => "&#8226; Про мене"
+	);
+	
+		foreach ($main_nav as $key => $value) {
+			echo "<a href='$key'>$value</a> " ;
+	
+		}
+	
+?>
+
+</header>
